c4e90718e990fc64a5cbb733eb2dec5cd1916416
Eloy Dominguez <eloydominguez3@gmail.com>
2026-02-17 12:15:07 +0100

    Modulo de phyton hecho

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,34 @@
-fn main() {
-    println!("Hello, world!");
+use pyo3::prelude::*;
+use gpx::read;
+use std::fs::File;
+use std::io::BufReader;
+
+// Esta es la función que Python verá
+#[pyfunction]
+fn leer_coordenadas(ruta_gpx: String) -> PyResult<Vec<(f64, f64)>> {
+    // 1. Abrimos el archivo
+    let file = File::open(ruta_gpx).map_err(|e| PyErr::new::<pyo3::exceptions::PyFileNotFoundError, _>(e.to_string()))?;
+    let reader = BufReader::new(file);
+
+    // 2. Procesamos el GPX
+    let gpx_data = read(reader).map_err(|_| PyErr::new::<pyo3::exceptions::PyValueError, _>("El archivo GPX está corrupto"))?;
+    
+    let mut coords = Vec::new();
+
+    for track in gpx_data.tracks {
+        for segment in track.segments {
+            for p in segment.points {
+                coords.push((p.point().y(), p.point().x()));
+            }
+        }
+    }
+
+    Ok(coords)
+}
+
+// Aquí registramos el módulo para Python
+#[pymodule]
+fn procesador_gpx(_py: Python, m: &PyModule) -> PyResult<()> {
+    m.add_function(wrap_pyfunction!(leer_coordenadas, m)?)?;
+    Ok(())
 }
